3112da573b40981485720687141dcbfe56c43fc6
tanmaypatil <tanmaypatil.620@gmail.com>
2015-06-30 12:03:28 +0530

    merged unixfiles folder and updated gitattribbutes

--- unixfiles/tst.py
+++
@@ -1,8 +0,0 @@
-import os;
-x=input("enter the value to speed down animation by multiple of 1/100th of second(enter 12 for normal speed)(enter 0 for max speed) ")
-def fun(x):
-	
-	os.system("povray fred.ini");
-	os.system("animate -delay "+str(x)+" fredexp*.png");
-
-fun(x)
