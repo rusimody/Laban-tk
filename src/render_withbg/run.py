ba94ce12dd164beea0590e029fc6f95b35af482a
Samir Patil <answersamir@gmail.com>
2015-07-17 08:04:53 -0700

    unified nudes pipeline  and render for debugging purposes

--- src/render_withbg/run.py
+++ src/render_withbg/run.py
@@ -1,12 +1,12 @@
 import subprocess
-import os
+"""import os
 # runs led to create lbn file
 p = subprocess.Popen("./led")
 p.wait()
 # runs lintel to create .n file
 p = subprocess.Popen("./lintel")
 p.wait()
-
+"""
 # gets the name of .n file to be processed
 # removes allow commands from the file which cause trobule
 userinput = raw_input("Enter the name of .n file created Eg(test.lbn_000.n)" )
@@ -24,20 +24,5 @@ f.close()
 p = subprocess.Popen(['./nudes',userinput])
 p.wait()
 
-# go into the newly created folder and create a gif using imagemagick
-folder = userinput[:-1] + "tmp"
-currentpath = os.getcwd()
-path = currentpath+"/"+folder
-os.chdir(path)
-initials = userinput[:5]
-command = "convert -delay 10 -loop 0 "+initials+"*.ppm animation.gif"
-print (command)
-print ("GIF is being generated .......")
-p = subprocess.Popen(command,shell=True)
-p.wait()
-command = "rm *.ppm"
-p = subprocess.Popen(command,shell=True)
-p.wait()
-print("All done!!!! find animation.gif in "+folder+" folder")
 
 
