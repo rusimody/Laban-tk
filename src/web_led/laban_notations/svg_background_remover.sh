7ff80291df0cf2d994a4287d3a800a7556f0366b
Sumit Kukade <kukade4@gmail.com>
2017-04-07 05:35:59 +0000

    laban notations refactored

--- src/web_led/laban_notations/svg_background_remover.sh
+++ src/web_led/laban_notations/svg_background_remover.sh
@@ -1,1 +1,11 @@
-sed -i '/^<path fill=\"#ffffff\"/d' *
+sed -i '/^<path fill=\"#ffffff\"/d' body/*
+
+sed -i '/^<path fill=\"#ffffff\"/d' direction/*
+
+sed -i '/^<path fill=\"#ffffff\"/d' intensity*
+
+sed -i '/^<path fill=\"#ffffff\"/d' others/*
+
+sed -i '/^<path fill=\"#ffffff\"/d' rotation/*
+
+sed -i '/^<path fill=\"#ffffff\"/d' turning/*
